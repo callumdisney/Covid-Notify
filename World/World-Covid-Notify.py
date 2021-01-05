3c41bcb88d29a133c0e81e66bac8c87e607d07aa
Callum Disney <github@callumdisney.tk>
2021-01-05 11:56:14 +0000

    Update World-Covid-Notify.py

--- World/World-Covid-Notify.py
+++ World/World-Covid-Notify.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
-# Desktop notifier built for updates for the covid pandemic in World
+# Desktop notifier built for updates for the covid pandemic in the World
 # Made by traditionallimb - Refined and parts added by Callum Disney
 # Made 10.11.20
 # Refined and parts added 04.01.21
